aef59f2c200db22692a6ceed20d002cdb6a63357
ZIMU <59822264+lipai-ustc@users.noreply.github.com>
2024-04-03 13:07:21 +0800

    Update test.sh

--- examples/test.sh
+++ examples/test.sh
@@ -7,32 +7,32 @@ do
 done
 
 cd 0.all
-python ../../lipai_data.py -i -k -p --dim=1 1 0 --opt --md --ML --Econverge --dos --workfunction --bader --ELF --spin --pbeU --mix --chout --r2scan --addcharge 2.3 --STM --BKcharge --phonon --vibration
+python ../../vkit -i -k -p --dim=1 1 0 --opt --md --ML --Econverge --dos --workfunction --bader --ELF --spin --pbeU --mix --chout --r2scan --addcharge 2.3 --STM --BKcharge --phonon --vibration
 
 cd ../1.normal
-python ../../lipai_data.py -i -k -p --dim=1 1 0 --STM
+python ../../vkit -i -k -p --dim=1 1 0 --STM
 
 cd ../2.opt
-python ../../lipai_data.py -i -k -p --opt --accu=Low --dim=1 1 0
+python ../../vkit -i -k -p --opt --accu=Low --dim=1 1 0
 
 cd ../3*
-python ../../lipai_data.py -i -k -p --spin --dim=1 1 0 --dos
+python ../../vkit -i -k -p --spin --dim=1 1 0 --dos
 
 cd ../4*
-python ../../lipai_data.py -i -k -p --spin --r2scan --dim=1 1 0
+python ../../vkit -i -k -p --spin --r2scan --dim=1 1 0
 
 cd ../5*
-python ../../lipai_data.py -i -k -p --spin --r2scan --pbeU --dim=1 1 0
+python ../../vkit -i -k -p --spin --r2scan --pbeU --dim=1 1 0
 
 cd ../6*
-python ../../lipai_data.py opt dos --dim=1 1 0
+python ../../vkit opt dos --dim=1 1 0
 
 cd ../7*
-python ../../lipai_data.py opt phonon --dim=1 1 0
+python ../../vkit opt phonon --dim=1 1 0
 
 cd ../8*
-python ../../lipai_data.py opt vibration --dim=1 1 0
+python ../../vkit opt vibration --dim=1 1 0
 
 cd ../9.spin+pbeU+args_opt_dos
-python ../../lipai_data.py opt dos --spin --pbeU  --dim=1 1 0
+python ../../vkit opt dos --spin --pbeU  --dim=1 1 0
 
